3ae23a027483db67a72ca2535c5bc929e9c012b6
nikhil-tej-arepally <2400030129@kluniversity.in>
2026-05-12 19:22:22 +0530

    Updated project

--- final.py
+++ final.py
@@ -1,61 +1,50 @@
-import glob
 import os
+import glob
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
 embeddings=OllamaEmbeddings(model="nomic-embed-text")
-if os.path.exists("faiss_db"):
+
+if os.path.exists("fais_db"):
     vs=FAISS.load_local(
         "faiss_db",
         embeddings,
         allow_dangerous_deserialization=True
-
     )
 else:
-    all_docs = []
-    pdf_files = glob.glob("data/*.pdf")
+    all_doc=[]
+    pdf_file=glob.glob("data/*.pdf")
+    for pdf in pdf_file:
+        loader=PyMuPDFLoader(pdf)
+        final_pdf=loader.load()
+        all_doc.extend(final_pdf)
 
-    for pdf in pdf_files:
-        loader = PyMuPDFLoader(pdf)
-        doc = loader.load()
-        all_docs.extend(doc)
-
-    splitter = RecursiveCharacterTextSplitter(
+    splitter=RecursiveCharacterTextSplitter(
         chunk_size=200,
         chunk_overlap=50
     )
-    chunks = splitter.split_documents(all_docs)
-    vs = FAISS.from_documents(
+    chunks=splitter.split_documents(all_doc)
+    vs=FAISS.from_documents(
         chunks,
         embeddings
     )
-    vs.save_local("faiss_db")
+    vs.save_local("fais_db")
 
-retriever=vs.as_retriever()
+retriever=vs.as_retriever(search_kwargs={"k": 3})
 
 llm=OllamaLLM(model="phi3")
-
 prompt=PromptTemplate(
     input_variables=["context", "question"],
-    template="""
-Answer ONLY using the provided context.
-
-If answer is not found, say:
-"I could not find that in the documents."
-
-Context:
-{context}
-
-Question:
-{question}
-"""
+    template="""Answer only from the give context. if answer is not found, say i couldn't found that in document
+    context: {context}
+    question: {question}
+    """
 )
-
 chain=(
     {
         "context":retriever,
@@ -64,13 +53,19 @@ chain=(
     | prompt
     | llm
     | StrOutputParser()
-)
 
+)
 while True:
     question=input("You: ")
     if question.lower()=="quit":
         break
 
     response=chain.invoke(question)
+    print(response)
+    retrieved_doc=retriever.invoke(question)
+    for doc in retrieved_doc:
+        print(f"source: {doc.metadata["source"]}")
+        print(f"page: {doc.metadata["page"]}")
 
-    print(f"AI: {response}")
+
+
